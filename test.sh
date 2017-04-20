f09cbf215868c39a7543e8ca8bee9be9c35b7eb4
Bastian Köcher <git@kchr.de>
2017-04-20 20:22:51 +0200

    Adds patch for codeclimate-cli to work in circleci

--- test.sh
+++ test.sh
@@ -3,8 +3,12 @@ set -e
 
 docker run -it -v $(pwd)/src:/src rust-clippy-build /bin/sh -c "cd codeclimate-clippy && cargo test"
 
-curl -L https://github.com/codeclimate/codeclimate/archive/master.tar.gz | tar xvz
-cd codeclimate-* && sudo make install && cd ..
+git clone https://github.com/codeclimate/codeclimate.git codeclimate-cli-src
+cd codeclimate-cli-src
+git checkout 13bc36548f6713b73332d144a3470aa37fcf8e1
+git apply ../codeclimate-engine.patch
+sudo make install
+cd ..
 
 docker build -t codeclimate/codeclimate-rust-clippy --rm=false .
 
