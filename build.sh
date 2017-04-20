a16219980d600d186a5ac94110cd50eb47826612
Bastian Köcher <git@kchr.de>
2017-04-20 18:25:43 +0200

    Updates the build script

--- build.sh
+++ build.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
+set -e
 
-docker build -trust-clippy-build -f Build.dockerfile .
+docker build -t rust-clippy-build -f Build.dockerfile .
 
 docker run -it -v $(pwd)/src:/src rust-clippy-build /bin/sh -c "cd rust-clippy && cargo build --release && cd ../codeclimate-clippy && cargo build --release"
 
 mkdir -p bin
 cp src/rust-clippy/target/release/cargo-clippy bin/
 cp src/codeclimate-clippy/target/release/codeclimate-clippy bin/
+
