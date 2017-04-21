6da575807e93ae1c8b302f28a650aefff81790ec
Bastian Köcher <git@kchr.de>
2017-04-21 15:28:02 +0200

    Improves the deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 set -e
 
-GIT_COMMIT_MSG="New rust-clippy engine update\n\nUpdate based on commit https://github.com/bkchr/codeclimate-rust-clippy-src/commit/$(git rev-parse HEAD)"
+GIT_COMMIT_MSG_HEADER="New rust-clippy engine update"
+GIT_COMMIT_MSG_CONTENT="Update based on commit https://github.com/bkchr/codeclimate-rust-clippy-src/commit/$(git rev-parse HEAD)"
 
 git clone git@github.com:bkchr/codeclimate-rust-clippy.git codeclimate-rust-clippy
 cd codeclimate-rust-clippy
@@ -17,5 +18,5 @@ cp -f ../main.sh .
 cp -f ../install-rust.sh .
 
 git add .
-git commit -am "$GIT_COMMIT_MSG"
+git commit -am "$GIT_COMMIT_MSG_HEADER" -m "$GIT_COMMIT_MSG_CONTENT"
 git push
